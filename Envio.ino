33c616e95139295a5e030997e77b62b2846ff21b
TUDOOUNADA <115155378+TUDOOUNADA@users.noreply.github.com>
2024-05-14 00:12:05 -0300

    Update Envio.ino

--- Envio.ino
+++ Envio.ino
@@ -7,3 +7,25 @@ void Task1code( void * pvParameters )
       loop_2 ();
     }
 }
+
+//Ciclo de Ventilação
+void Ciclo(){
+if (VT1 <= 600) {
+      digitalWrite(gpio_E1, 0);
+      E_State = false;
+      digitalWrite(gpio_V1, 1);
+      V_State = true;
+    }
+
+    else if (VT1 > 600) {
+      digitalWrite(gpio_V1, 0);
+      V_State = false;
+      digitalWrite(gpio_E1, 1);
+      E_State = true;
+    }
+
+    else if (VT1 > 1200) {
+      VT1 = 0;
+      }
+    VT1++;
+  }
